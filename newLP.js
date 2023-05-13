6db3c1e0007a5c3b6ec668b52052b70920b0717a
Vlad-syss <123962761+Vlad-syss@users.noreply.github.com>
2023-05-13 18:10:01 +0300

    Add files via upload

---
+++ newLP.js
@@ -0,0 +1,14 @@
+new Swiper('._container_header', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    keyboard: {
+        enabled: true,
+    }
+});
